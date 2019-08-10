d9a994b14a02de055b84e74e3a5fbce07543d3ee
Estellise <shiranoyukihime@gmail.com>
2019-08-10 16:51:28 +0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,37 @@
+# Graduation Photo Books
+
+## Install
+
+``` bash
+[//] # checkout to production
+[//] # git checkout production
+
+# Install Dependencies
+composer install
+
+[//] # Run Migrations
+[//] # php artisan migrate
+
+# Add virtual host if using Apache
+
+[//] # If you get an error about an encryption key
+[//] # php artisan key:generate
+
+# If no .env file
+cp .env-example .env
+
+# Install JS Dependencies
+npm install
+
+# Watch Files
+npm run watch
+
+# Serve
+php artisan serve
+
+or
+
+# If have virtual host
+localhost/graduationbook/public / graduationbook.co
+
+```
---
+++ resources/js/components/Login/Login.vue
@@ -0,0 +1,133 @@
+<template>
+    <div>
+        <div class="background">
+          <router-view/>
+          <img v-for="image in images" :key="image.id" :src="image.src" ref="images" alt="image">
+        </div>
+    </div>
+</template>
+
+<script>
+import { setTimeout } from 'timers';
+    export default {
+        data(){
+            return{
+              images: []
+            };
+        },
+        mounted: function(){
+
+            console.log(this.$refs);
+
+            let images = [
+              {
+                id: "img1",
+                src: "storage/img/login-background/1.jpg",
+              },
+              {
+                id: "img2",
+                src: "storage/img/login-background/2.jpg"
+              },
+              {
+                id: "img3",
+                src: "storage/img/login-background/3.jpg"
+              }
+            ];
+
+            this.images = Object.assign([], images).reverse();
+
+            this.$nextTick(function(){
+              this.backgroundChanger(this.$refs.images, images);
+            });
+        },
+        methods: {
+          backgroundChanger: function(e, images = []) {
+
+            /**/
+            window.clearTimeout();
+
+            let seconds = 10;
+            let length = images.length;
+
+            let sequence = function(){
+                let x = 0;
+
+                for(let i = 0; i < length; i++){
+                    setTimeout(function(){
+                      
+                      let last = i === 0 ? length - 1 : i - 1;
+                      e[last].style.opacity = 0;
+                      e[i].style.opacity = 1;
+
+                      if((x + 1) === images.length){
+                          setTimeout(function(){
+                              sequence();
+                          }, (seconds * 1000));
+                      }
+                      else{
+                          x++;
+                      }
+                    }, (seconds * 1000) * i);
+                }
+            }
+
+            sequence();
+            /**/
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+/* STRUCTURE */
+.background{   
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  
+  background-blend-mode: darken;
+
+  min-height: 100%;
+  /* Set up proportionate scaling */
+  width: 100%;
+      
+  /* Set up positioning */
+  position: fixed;
+  top: 0;
+  left: 0;
+  
+  background-image: url("https://i.pinimg.com/originals/3c/7a/fc/3c7afc1b68c0f8cc367dd9d0f1f383de.jpg");
+  
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
+}
+
+.background > img{
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  position: fixed;
+  top: 0;
+  left: 0;
+  min-height: 100%;
+  width: 100%;
+  z-index: -1;
+
+  opacity: 0;
+
+  -webkit-transition: opacity 1s ease-in-out;
+  -moz-transition: opacity 1s ease-in-out;
+  -ms-transition: opacity 1s ease-in-out;
+  -o-transition: opacity 1s ease-in-out;
+  transition: opacity 1s ease-in-out;
+
+}
+
+
+  
+
+</style>
